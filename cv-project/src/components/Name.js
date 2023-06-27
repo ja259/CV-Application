5c38cd280d9494e0303e8c9661cb8c274f3194cc
James Otwane <127046977+ja259@users.noreply.github.com>
2023-06-27 19:12:10 -0400

    Add  src/components/Name.js

--- cv-project/src/components/Name.js
+++ cv-project/src/components/Name.js
@@ -6,7 +6,7 @@ import Interests from './Interests';
 import NameInput from './NameInput';
 import ContactInput from './ContactInput';
 import SocialInput from './SocialInput';
-import InteInput from './IntereInput';
+import IntereInput from './IntereInput';
 
 class Name extends React.Component {
     constructor(props) {
